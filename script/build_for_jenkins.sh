ede6f25986b3a0290397d82ab2d4cbb27c7b6333
sue445 <sue445@sue445.net>
2013-02-03 23:56:07 +0900

    refactor jenkins script

--- script/build_for_jenkins.sh
+++ script/build_for_jenkins.sh
@@ -1,13 +1,47 @@
 #!/bin/bash
 
-readonly RAILS_ENV="test"
+readonly JENKINS_RAILS_ENV="test"
 
 # Jenkins build script
-gem install bundler --no-ri --no-rdoc
-RAILS_ENV=${RAILS_ENV} bundle install --path vendor/bundle
-RAILS_ENV=${RAILS_ENV} bundle exec rake db:create
-RAILS_ENV=${RAILS_ENV} bundle exec rake db:migrate
-RAILS_ENV=${RAILS_ENV} bundle exec rspec
 
-rm -rf reports
-mkdir -m 777 reports/
+run()
+{
+  command=$1
+  echo "$command"
+
+  eval $command
+
+  # if error code returned, exit this script with error code
+  RET=$?
+  if [ $RET -ne 0 ]; then
+    exit $RET
+  fi
+}
+
+run "cp config/database.yml.jenkins config/database.yml"
+run "gem install bundler --no-ri --no-rdoc"
+
+
+#########################
+echo "bundle install --path vendor/bundle"
+
+bundle install --path vendor/bundle
+
+RET=$?
+if [ $RET -ne 0 ]; then
+  # if failed 'bundle install', run 'bundle update'
+  run "bundle update"
+fi
+
+run "RAILS_ENV=${JENKINS_RAILS_ENV} bundle exec rake db:create"
+run "RAILS_ENV=${JENKINS_RAILS_ENV} bundle exec rake db:migrate"
+
+# if you use mysql_partitioning, use don't use "rake spec". because primary key is dropped
+#run "RAILS_ENV=${JENKINS_RAILS_ENV} bundle exec rake ci:setup:rspec spec"
+
+run "RAILS_ENV=${JENKINS_RAILS_ENV} bundle exec rspec"
+
+run "rm -rf reports"
+run "mkdir -m 777 reports/"
+
+exit 0
