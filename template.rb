c510bf4e36a72b7ff6d033dff2237a513c3a9a7b
sue445 <sue445@sue445.net>
2013-08-01 14:51:10 +0900

    refactor
    remove assets group
    
    remove scaffolds.css

--- template.rb
+++ template.rb
@@ -5,6 +5,9 @@ else
 end
 
 gems = {}
+gems[:capistrano] = yes? "Would you like to install capistrano?"
+gems[:jenkins]    = yes? "Would you like to install Jenkins CI tools?"
+gems[:bootstrap]  = yes? "Would you like to install twitter-bootstrap-rails?"
 
 def copy_from_repo(path)
   get "#{REPO_URL}/#{path}", path
@@ -66,7 +69,7 @@ end
   EOS
 end
 
-if yes? "Would you like to install Jenkins CI tools?"
+if gems[:jenkins]
   label "Jenkins CI"
   gem_group :test do
     gem "simplecov", :require => false
@@ -88,8 +91,6 @@ if yes? "Would you like to install Jenkins CI tools?"
   insert_into_file "Rakefile", "require 'ci/reporter/rake/rspec' if Rails.env.test?\n", :after => "require File.expand_path('../config/application', __FILE__)\n"
 end
 
-gems[:capistrano] = yes? "Would you like to install capistrano?"
-
 if gems[:capistrano]
   label "Deploy with Capistrano"
   gem_group :development do
@@ -102,22 +103,17 @@ if gems[:capistrano]
   end
 end
 
-gems[:bootstrap] = yes? "Would you like to install twitter-bootstrap-rails?"
 
 if gems[:bootstrap]
   label "twitter-bootstrap-rails"
 
-  gem_group :assets do
-    gem "less-rails"
-    gem "libv8", "~> 3.11.8"
-    gem "twitter-bootstrap-rails", ">= 2.1.3"
-    gem "therubyracer", ">= 0.10.2", :platform => :ruby
-  end
+  gem "less-rails"
+  gem "libv8", "~> 3.11.8"
+  gem "twitter-bootstrap-rails", ">= 2.1.3"
+  gem "therubyracer", ">= 0.10.2", :platform => :ruby
 end
 
 remove_file "test/"
-
-
 copy_from_repo "spec/spec_helper.rb"
 copy_from_repo "spec/factories/sequences.rb"
 copy_from_repo ".rspec"
@@ -148,7 +144,7 @@ capify! if gems[:capistrano]
 if gems[:bootstrap]
   generate 'bootstrap:install'
 
-  if yes?("Would you like to create FIXED layout?(yes=FIXED, no=FLUID)")
+  if yes? "Would you like to create FIXED layout?(yes=FIXED, no-FLUID)"
     generate 'bootstrap:layout application fixed -f'
   else
     generate 'bootstrap:layout application fluid -f'
@@ -156,6 +152,8 @@ if gems[:bootstrap]
 
   get "https://gist.github.com/sue445/5261654/raw/ja.bootstrap.yml", "config/locales/ja.bootstrap.yml"
   #gsub_file "app/views/layouts/application.html.haml", /lang="en"/, %(lang="ja")
+
+  remove_file "app/assets/stylesheets/scaffolds.css.scss"
 end
 
 run "bundle exec guard init"
