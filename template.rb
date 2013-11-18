ebfe79de9bf4f58ca2e01996ed1dccc0b8dbe577
sue445 <sue445@sue445.net>
2013-11-18 19:47:43 +0900

    remove regacy guard gems

--- template.rb
+++ template.rb
@@ -48,11 +48,6 @@ append_to_file "Gemfile" do
 group :development do
   gem 'guard-rspec'
 
-  # ref. https://github.com/guard/guard#efficient-filesystem-handling
-  gem 'rb-inotify', :require => false
-  gem 'rb-fsevent', :require => false
-  gem 'rb-fchange', :require => false
-
   # Runs on Mac OS X
   gem 'growl'
 
