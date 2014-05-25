332d2e20ab22782daacf568377d2f7a6485ded39
sue445 <sue445@sue445.net>
2014-05-25 23:39:13 +0900

    Remove legacy wrapper

--- script/build_for_jenkins.sh
+++ script/build_for_jenkins.sh
@@ -1,56 +1,39 @@
-#!/bin/bash
+#!/bin/bash -xe
 
 readonly JENKINS_RAILS_ENV="test"
 export LANG=ja_JP.UTF-8
 
 # Jenkins build script
-
-run()
-{
-  command=$1
-  echo "$command"
-
-  eval $command
-
-  # if error code returned, exit this script with error code
-  RET=$?
-  if [ $RET -ne 0 ]; then
-    exit $RET
-  fi
-}
-
-run "which ruby"
-run "ruby --version"
-run "cp config/database.yml.jenkins config/database.yml"
-run "gem install bundler --no-ri --no-rdoc"
+which ruby
+ruby --version
+cp config/database.yml.jenkins config/database.yml
+gem install bundler --no-ri --no-rdoc
 
 
 #########################
-echo "bundle install --path vendor/bundle"
-
 bundle install --path vendor/bundle
 
 RET=$?
 if [ $RET -ne 0 ]; then
   # if failed 'bundle install', run 'bundle update'
-  run "bundle update"
+  bundle update
 fi
 
-run "rm log/*.log"
-run "bundle clean"
+rm log/*.log
+bundle clean
 
-run "RAILS_ENV=${JENKINS_RAILS_ENV} bundle exec rake db:create"
-run "RAILS_ENV=${JENKINS_RAILS_ENV} bundle exec rake db:migrate"
+RAILS_ENV=${JENKINS_RAILS_ENV} bundle exec rake db:create
+RAILS_ENV=${JENKINS_RAILS_ENV} bundle exec rake db:migrate
 
-run "rm -rf reports"
-run "mkdir -m 777 reports/"
+rm -rf reports
+mkdir -m 777 reports/
 
-run "RAILS_ENV=${JENKINS_RAILS_ENV} bundle exec rspec --profile > ./reports/rspec-console.log"
-run "ruby ./script/plot-rspec-slowest-examples.rb ./reports/rspec-console.log > ./reports/rspec-plot.csv"
+RAILS_ENV=${JENKINS_RAILS_ENV} bundle exec rspec --profile > ./reports/rspec-console.log
+ruby ./script/plot-rspec-slowest-examples.rb ./reports/rspec-console.log > ./reports/rspec-plot.csv
 
 # * if you use mysql_partitioning, use don't use `rake spec`. because primary key is dropped
 # * `rake spec` can not use --profile
-#run "RAILS_ENV=${JENKINS_RAILS_ENV} bundle exec rake ci:setup:rspec spec"
+#RAILS_ENV=${JENKINS_RAILS_ENV} bundle exec rake ci:setup:rspec spec
 
 
 exit 0
